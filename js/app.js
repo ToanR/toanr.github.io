1ff8bfd8caa2d232df46aecf0663e61b092c5926
toanr <minhtoan.jc@gmail.com>
2019-11-13 17:19:03 +0700

    init

---
+++ js/app.js
@@ -0,0 +1,10 @@
+var typed = new Typed("#typed", {
+  stringsElement: "#typed-strings",
+  typeSpeed: 60,
+  backSpeed: 70,
+  backDelay: 1500,
+  loop: true,
+  loopCount: 0,
+  showCursor: true,
+  cursorChar: "|"
+});
