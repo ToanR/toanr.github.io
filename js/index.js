e36389a613937dcf34d766e9381c1cc12ab32da8
toanr <minhtoan.jc@gmail.com>
2019-11-21 16:01:19 +0700

    update

--- js/index.js
+++ js/index.js
@@ -1,23 +1,6 @@
-function run(interval) {
-  var int = 0;
-  var increase = true;
+function run() {
   var headerImg = document.querySelector(".header__img");
-  function func() {
-    console.log(int);
-    headerImg.style.filter = `saturate(${int}%)`;
-    if (increase) {
-      int += 50;
-    } else {
-      int -= 50;
-    }
-    if (int === 100) {
-      increase = false;
-    }
-    if (int === 0) {
-      increase = true;
-    }
-  }
-  var swap = window.setInterval(func, interval);
+  headerImg.style.filter = `saturate(100%)`;
 }
 
-run(5000);
+run();
