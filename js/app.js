2aebf21c0c105d7bf054372b2f08e66ecdde9253
toanr <minhtoan.jc@gmail.com>
2019-11-21 14:36:02 +0700

    update

--- js/app.js
+++ js/app.js
@@ -1,10 +1,26 @@
 var typed = new Typed("#typed", {
   stringsElement: "#typed-strings",
-  typeSpeed: 60,
-  backSpeed: 70,
+  typeSpeed: 90,
+  backSpeed: 80,
   backDelay: 1500,
   loop: true,
   loopCount: 0,
   showCursor: true,
   cursorChar: "|"
 });
+
+function run(interval, frames) {
+  var int = 1;
+  var headerImg = document.querySelector(".header__img");
+  console.log(headerImg);
+  function func() {
+    headerImg.style.backgroundImage = `url('../img/image-${int}.jpg')`;
+    int++;
+    if (int === frames) {
+      int = 1;
+    }
+  }
+  var swap = window.setInterval(func, interval);
+}
+
+run(5000, 5);
