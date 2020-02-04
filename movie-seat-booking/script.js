0a7512e6268f95b3844aaf7eca1ff152ce8f4222
Minh Toan <minhtoan.jc@gmail.com>
2020-02-04 17:14:17 +0700

    moive-booking

---
+++ movie-seat-booking/script.js
@@ -0,0 +1,38 @@
+const container = document.querySelector('.container');
+const seats = document.querySelectorAll('.row .seats:not(.occupied)');
+
+const count = document.getElementById('count');
+const total = document.getElementById('total');
+
+const movieSelect = document.getElementById('movie');
+console.log(movieSelect.value);
+
+let ticketPrice = +movieSelect.value;
+
+// update count and total
+function updateSelectedCount() {
+  const selectedSeats = document.querySelectorAll('.row .seat.selected');
+
+  const selectedSeatsCount = selectedSeats.length;
+  console.log(selectedSeatsCount);
+  count.innerText = selectedSeatsCount;
+  total.innerText = selectedSeatsCount * ticketPrice;
+}
+// movie select event
+movieSelect.addEventListener('change', e => {
+  ticketPrice = +e.target.value;
+  updateSelectedCount();
+});
+
+// Seat click event
+container.addEventListener('click', e => {
+  if (
+    e.target.classList.contains('seat') &&
+    !e.target.classList.contains('occupied')
+  ) {
+    e.target.classList.toggle('selected');
+    updateSelectedCount();
+  }
+});
+
+console.log(typeof ticketPrice);
