e14565f45d4d6c85160219cdb1295f4643f0c670
Minh Toan <minhtoan.jc@gmail.com>
2020-02-04 08:45:19 +0700

    update

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,5 +1,5 @@
 function run() {
-  var headerImg = document.querySelector(".header__img");
+  var headerImg = document.querySelector('.header__img');
   headerImg.style.filter = `saturate(100%)`;
 }
 
